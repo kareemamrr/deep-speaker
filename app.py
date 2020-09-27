32425c84ce5f518b098176c0da3ba34ae1215a17
Kareem Amr <kareem@pop-os.localdomain>
2020-09-27 22:30:15 +0200

    added streamlit UI

---
+++ app.py
@@ -0,0 +1,53 @@
+from backend import get_embedding
+from backend import verify_identity
+
+import os
+import random
+
+import streamlit as st
+import numpy as np
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+st.title("Speaker Verification")
+st.set_option("deprecation.showfileUploaderEncoding", False)
+
+np.random.seed(123)
+random.seed(123)
+
+database = {}
+
+menu_option = st.sidebar.radio("Menu", ["Enroll user", "Test user"])
+
+if menu_option == "Enroll user":
+    st.subheader(
+        "Enroll a new user by entering their name and upload a short audio file of speech."
+    )
+    username = st.text_input("User name", "")
+    audio_file = st.file_uploader(
+        "Upload a short wav or flac file", type=(["wav", "flac"])
+    )
+    st.audio(audio_file)
+    if username != "" and audio_file is not None:
+        if st.button("Enroll"):
+            with st.spinner("Extracting embedding..."):
+                embedding = get_embedding(audio_file)
+                database["username"] = embedding
+            st.success("Embedding extracted successfully")
+
+else:
+    st.subheader(
+        "Verify your identity by typing in your name and uploading a short audio file of speech (different from the one used for enrolling."
+    )
+    username = st.text_input("User name", "")
+    audio_file = st.file_uploader(
+        "Upload a short wav or flac file", type=(["wav", "flac"])
+    )
+    st.audio(audio_file)
+    if username != "" and audio_file is not None:
+        if st.button("Verify"):
+            with st.spinner("Verifiying identity"):
+                result = verify_identity(database, audio_file, username)
+            if result:
+                st.success("Identity verified successfully")
+            else:
+                st.error("Identity not verified")
---
+++ backend.py
@@ -0,0 +1,32 @@
+import random
+import numpy as np
+from audio import read_mfcc
+from batcher import sample_from_mfcc
+from constants import SAMPLE_RATE, NUM_FRAMES
+from conv_models import DeepSpeakerModel
+from test import batch_cosine_similarity
+
+np.random.seed(123)
+random.seed(123)
+
+
+def get_embedding(audio_file):
+    model = DeepSpeakerModel()
+    path = "ResCNN_checkpoint_850.h5"
+
+    model.m.load_weights(path, by_name=True)
+
+    mfcc = sample_from_mfcc(read_mfcc(audio_file, SAMPLE_RATE), NUM_FRAMES)
+
+    embedding = model.m.predict(np.expand_dims(mfcc, axis=0))
+    return embedding
+
+
+def verify_identity(database, audio_file, username):
+    identity_embedding = database[username]
+    test_embedding = get_embedding(audio_file)
+    similarity = batch_cosine_similarity(identity_embedding, test_embedding)
+    if similarity > 0.7:
+        return 1
+    else:
+        return 0
